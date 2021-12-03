e2388d6177e4f448a8b3ffa28b1598e134069e2d
Jemimah <jpfilarca@gmail.com>
2021-12-04 00:00:49 +0100

    fixed submit button

--- app/src/components/form/Form.js
+++ app/src/components/form/Form.js
@@ -2,6 +2,17 @@ import "./form.css"
 
 export default function Form () {
     return(
-        <form className="form">Form</form>
+        <div className="formContainer">
+            <form className="form">
+                <div className="formInputContainer">                    
+                    <input className="formInput" type="text" value="Shut up:"/>
+                    <input className="formInput"type="text" value="and take my money:"/>
+                </div>
+                <input className="formSubmit"type="submit" value="Get a new meme image  🖼" />
+            </form>
+            <div className="meme">
+                <img src="memeimg.png" alt=""></img>
+            </div>
+        </div>
     )
 }
