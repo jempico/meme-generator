325550cc4b870e56b419bf21e305d6d1661d1e14
Jemimah <jpfilarca@gmail.com>
2021-12-04 00:14:40 +0100

    finished UI

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# meme-generator
+# Figma file: https://www.figma.com/file/yYr9AXxXo5hmMVJu9vhBhK/Meme-Generator-(Copy)?node-id=2%3A2
+
--- app/src/components/footer/Footer.js
+++ app/src/components/footer/Footer.js
@@ -2,6 +2,9 @@ import "./footer.css"
 
 export default function Footer () {
     return(
-        <footer className="footer">Footer</footer>
+        <footer className="footer">
+            ©<a href="jempico.com">Jemimah Pico</a>
+            , 2021
+        </footer>
     )
 }
