85e3aa33568cec956448e047c1e82cba788a52f9
Jemimah <jpfilarca@gmail.com>
2021-12-09 19:28:34 +0100

    fixed form button

--- app/src/components/form/Form.js
+++ app/src/components/form/Form.js
@@ -3,13 +3,13 @@ import "./form.css"
 export default function Form () {
     return(
         <main className="formContainer">
-            <form className="form">
+            <div className="form">
                 <div className="formInputContainer">                    
                     <input className="formInput" type="text" placeholder="Shut up:"/>
                     <input className="formInput"type="text" placeholder="and take my money:"/>
                 </div>
                 <button className="formSubmit">Get a new meme image  🖼</button>
-            </form>
+            </div>
             <div className="meme">
                 <img src="memeimg.png" alt=""></img>
             </div>
